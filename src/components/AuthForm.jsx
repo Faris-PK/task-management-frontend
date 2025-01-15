b4bb4a4fefd85c3b04e7f6886956347fc71f730e
Faris Pk <faripk369@gmail.com>
2025-01-15 10:59:48 +0530

    confirm password validation done

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -28,8 +28,8 @@ const AuthForm = ({ type, onSubmit }) => {
     
     if (!formData.password) {
       newErrors.password = 'Password is required';
-    } else if (formData.password.length < 8) {
-      newErrors.password = 'Password must be at least 8 characters long';
+    } else if (formData.password.length < 6) {
+      newErrors.password = 'Password must be at least 6 characters long';
     }
     
     if (type === 'register') {
